389d417e32431b57b6e59ba012aed52b46e1f268
iblameaadityaaa <106476833+iblameaadityaaa@users.noreply.github.com>
2024-08-16 00:54:08 +0530

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,46 +1,65 @@
-#include<iostream>
+/*
+BUBBLE SORTING ALGORITHM:
+
+In Bubble Sort algorithm, 
+traverse from left and compare adjacent elements and the higher one is placed at right side. 
+In this way, the largest element is moved to the rightmost end at first. 
+This process is then continued to find the second largest and place it and so on until the data is sorted.
+
+Complexity Analysis of Bubble Sort:
+Time Complexity: O(N2)
+Auxiliary Space: O(1)
+
+Advantages of Bubble Sort:
+Bubble sort is easy to understand and implement.
+It does not require any additional memory space.
+It is a stable sorting algorithm, meaning that elements with the same key value maintain their relative order in the sorted output.
+requires a comparison opera
+Disadvantages of Bubble Sort:
+Bubble sort has a time complexity of O(N2) which makes it very slow for large data sets.
+Bubble sort is a comparison-based sorting algorithm,
+which means that it tor to determine the relative order of elements in the input data set. It can limit the efficiency of the algorithm in certain cases.
+*/
+// Optimized implementation of Bubble sort
+#include <bits/stdc++.h>
 using namespace std;
 
-void printArray(int arr[], int n) {
-    for(int i=0; i<n; i++) {
-        cout << arr[i] << " ";
+// An optimized version of Bubble Sort
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    bool swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        // If no two elements were swapped
+        // by inner loop, then break
+        if (swapped == false)
+            break;
     }
-    cout << endl;
 }
 
-void sortOne(int arr[], int n) {
-
-    int left = 0, right = n-1;
-
-    while(left < right) {
-
-        while(arr[left] == 0 && left < right )  {
-            left++;
-        }
-
-        while(arr[right]==1 && left < right){
-            right--;
-        }
-
-        //agar yha pohoch gye ho, iska matlab
-        //arr[left]==1 and arr[right]==0
-        if(left<right)
-        {
-            swap(arr[left], arr[right]);
-            left++;
-            right--;
-        }
-    }
-
+// Function to print an array
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << " " << arr[i];
 }
 
-int main() {
-
-    int arr[8] = {1,1,0,0,0,0,1,0};
-
-    sortOne(arr, 8);
-    printArray(arr, 8);
-
-
+// Driver program to test above functions
+int main()
+{
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+    int N = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, N);
+    cout << "Sorted array: \n";
+    printArray(arr, N);
     return 0;
 }
