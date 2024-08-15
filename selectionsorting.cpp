efee94732378ec1485dbebf3a5d54af5506bbbef
iblameaadityaaa <106476833+iblameaadityaaa@users.noreply.github.com>
2024-08-16 00:38:30 +0530

    Update selectionsorting.cpp

--- selectionsorting.cpp
+++ selectionsorting.cpp
@@ -1,16 +1,48 @@
-#include<vector>
+// C++ program for implementation of
+// selection sort
+#include <bits/stdc++.h>
 using namespace std;
-void selectionSort(vector<int>& arr, int n)
-{   
-    for(int i = 0; i < n-1; i++ ) {
-        int minIndex = i;
-        
-        for(int j = i+1; j<n; j++) {
-            
-            if(arr[j] < arr[minIndex]) 
-                minIndex = j;
-            
+
+// Function for Selection sort
+void selectionSort(int arr[], int n)
+{
+    // One by one move boundary of
+    // unsorted subarray
+    for (int i = 0; i < n - 1; i++)
+    {
+        // Find the minimum element in
+        // unsorted array
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
         }
-        swap(arr[minIndex], arr[i]);
+
+        // Swap the found minimum element
+        // with the first element
+        if (min_idx != i)
+            swap(arr[min_idx], arr[i]);
     }
 }
+
+// Function to print an array
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// Driver program
+int main()
+{
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    selectionSort(arr, n);
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
+
+
