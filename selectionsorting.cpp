27cbfa83cedb2675abb88c6fd9118b8c2ddb8a27
iblameaadityaaa <106476833+iblameaadityaaa@users.noreply.github.com>
2024-08-16 00:44:30 +0530

    Update selectionsorting.cpp

--- selectionsorting.cpp
+++ selectionsorting.cpp
@@ -1,5 +1,25 @@
-// C++ program for implementation of
-// selection sort
+/*
+Selection sort is a simple and efficient sorting algorithm that works by repeatedly selecting the smallest (or largest) 
+element from the unsorted portion of the list and moving it to the sorted portion of the list. 
+
+Complexity Analysis of Selection Sort:
+Time Complexity: The time complexity of Selection Sort is O(N^2) as there are two nested loops:
+One loop to select an element of Array one by one = O(N)
+Another loop to compare that element with every other Array element = O(N)
+Therefore overall complexity = O(N) * O(N) = O(N*N) = O(N^2)
+
+Auxiliary Space: O(1) as the only extra memory used is for temporary variables while swapping two values in Array. 
+The selection sort never makes more than O(N) swaps and can be useful when memory writing is costly. 
+
+Advantages of selection sort:
+Simple and easy to understand.
+Works well with small datasets.
+
+Disadvantages of selection sort algorithm 
+Selection sort has a time complexity of O(n^2) in the worst and average case.
+Does not work well on large datasets.
+Does not preserve the relative order of items with equal keys which means it is not stable.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
