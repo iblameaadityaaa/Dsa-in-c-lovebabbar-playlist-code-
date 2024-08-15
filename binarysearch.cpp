ed28023792b0f5500c4e3450d19f8fc9ff0e0773
iblameaadityaaa <106476833+iblameaadityaaa@users.noreply.github.com>
2024-08-15 23:04:32 +0530

    Update binarysearch.cpp

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,3 +1,44 @@
+/*
+What is Binary Search Algorithm?
+Binary search is a search algorithm used to find the position of a target value within a sorted array.
+It works by repeatedly dividing the search interval in half until the target value is found or the interval is empty. 
+The search interval is halved by comparing the target element with the middle value of the search space.
+
+Conditions to apply Binary Search Algorithm in a Data Structure:
+The data structure must be sorted.
+Access to any element of the data structure should take constant time.
+
+Binary Search Algorithm:
+Below is the step-by-step algorithm for Binary Search:
+Divide the search space into two halves by finding the middle index “mid”. 
+Compare the middle element of the search space with the key. 
+If the key is found at middle element, the process is terminated.
+If the key is not found at middle element, choose which half will be used as the next search space.
+If the key is smaller than the middle element, then the left side is used for next search.
+If the key is larger than the middle element, then the right side is used for next search.
+This process is continued until the key is found or the total search space is exhausted.
+
+Time Complexity: O(log N)
+Auxiliary Space: O(1)
+
+Applications of Binary Search Algorithm:
+Binary search can be used as a building block
+for more complex algorithms used in machine learning, such as algorithms for training neural networks or finding the optimal hyperparameters for a model.
+It can be used for searching in computer graphics such as algorithms for ray tracing or texture mapping.
+It can be used for searching a database.
+
+Advantages of Binary Search:
+Binary search is faster than linear search, especially for large arrays.
+More efficient than other searching algorithms with a similar time complexity, such as interpolation search or exponential search.
+Binary search is well-suited for searching large datasets that are stored in external memory, such as on a hard drive or in the cloud.
+
+Disadvantages of Binary Search:
+The array should be sorted.
+Binary search requires that the data structure being searched be stored in contiguous memory locations. 
+Binary search requires that the elements of the array be comparable, meaning that they must be able to be ordered.
+*/
+
+
 #include<iostream>
 using namespace std;
 
@@ -6,7 +47,7 @@ int binarySearch(int arr[], int size, int key) {
     int start = 0;
     int end = size-1;
 
-    int mid = start + (end-start)/2;
+    int mid = start + (end-start)/2;//(end-start)/2 is used to prevent integer overflow!
 
     while(start <= end) {
 
