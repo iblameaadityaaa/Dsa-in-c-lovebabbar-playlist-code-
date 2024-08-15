785e806ac2a1a316cb7c8b90e5d0bf4e176cd7e2
iblameaadityaaa <106476833+iblameaadityaaa@users.noreply.github.com>
2024-08-16 01:01:01 +0530

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -44,7 +44,6 @@ void bubbleSort(int arr[], int n)
             break;
     }
 }
-
 // Function to print an array
 void printArray(int arr[], int size)
 {
@@ -63,3 +62,70 @@ int main()
     printArray(arr, N);
     return 0;
 }
+
+/*
+INSERTION SORT ALGORITHM 
+Complexity Analysis of Insertion Sort :
+Time Complexity of Insertion Sort
+Best case: O(n) , If the list is already sorted, where n is the number of elements in the list.
+Average case: O(n 2 ) , If the list is randomly ordered
+Worst case: O(n 2 ) , If the list is in reverse order
+
+Space Complexity of Insertion Sort
+Auxiliary Space: O(1), Insertion sort requires O(1) additional space, making it a space-efficient sorting algorithm.
+
+Advantages of Insertion Sort:
+Simple and easy to implement.
+Stable sorting algorithm.
+Efficient for small lists and nearly sorted lists.
+Space-efficient.
+Adoptive. the number of inversions is directly proportional to number of swaps. For example, no swapping happens for a sorted array and it takes O(n) time only.
+
+Disadvantages of Insertion Sort:
+Inefficient for large lists.
+Not as efficient as other sorting algorithms (e.g., merge sort, quick sort) for most cases.
+*/
+// C++ program for implementation of Insertion Sort
+#include <iostream>
+using namespace std;
+
+/* Function to sort array using insertion sort */
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+
+        /* Move elements of arr[0..i-1], that are
+           greater than key, to one position ahead
+           of their current position */
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+/* A utility function to print array of size n */
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// Driver method
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
+
+
+
