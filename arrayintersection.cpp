db4b5fcdc31aa0fe56a237ab789bb734bec66dcd
iblameaadityaaa <106476833+iblameaadityaaa@users.noreply.github.com>
2024-08-15 22:22:56 +0530

    Update arrayintersection.cpp

--- arrayintersection.cpp
+++ arrayintersection.cpp
@@ -1,20 +1,30 @@
-#include<vector>
-using namespace std;
 /*
 we are gonna traverse both of the arrays and gonna check if element equals to the elements of arrays 
 and then we update it as int min and break.
 
 */
+#include<vector>
+using namespace std;
 vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
 {
 	int i = 0, j = 0;
     vector<int> ans;
-  for(int i=0; i<n; i++){
-    element=arr[i];
-     for(int j=0; j<m; j++){
-        if(arr[i]==arr[j]){
-             vector.push_back(element);
-		   arr[2]=INT_MIN;
-		     break;
-	}
-     }
+    while(i<n && j<m) {
+    
+    if(arr1[i]==arr2[j])
+    {
+    	ans.push_back(arr1[i]);
+        i++;
+        j++;
+    }
+    else if (arr1[i] < arr2[j]) {
+  		i++;
+    }
+    else
+    {
+    	j++;
+    }
+    
+    }
+    return ans;
+}
